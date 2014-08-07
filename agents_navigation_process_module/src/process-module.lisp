656a53a031e2195a0f5dbb189c82ca7e41825131
Yazdani <yazdani@cs.uni-bremen.de>
2014-08-07 19:20:59 +0200

    added pms and desigs

--- agents_navigation_process_module/src/process-module.lisp
+++ agents_navigation_process_module/src/process-module.lisp
@@ -1,7 +1,6 @@
-;;;
-;;; Copyright (c) 2010, Lorenz Moesenlechner <moesenle@in.tum.de>
+;;; Copyright (c) 2014, Fereshta Yazdani <yazdani@cs.uni-bremen.de>
 ;;; All rights reserved.
-;;; 
+;; 
 ;;; Redistribution and use in source and binary forms, with or without
 ;;; modification, are permitted provided that the following conditions are met:
 ;;; 
@@ -10,9 +9,10 @@
 ;;;     * Redistributions in binary form must reproduce the above copyright
 ;;;       notice, this list of conditions and the following disclaimer in the
 ;;;       documentation and/or other materials provided with the distribution.
-;;;     * Neither the name of Willow Garage, Inc. nor the names of its
-;;;       contributors may be used to endorse or promote products derived from
-;;;       this software without specific prior written permission.
+;;;     * Neither the name of the Institute for Artificial Intelligence/
+;;;       Universitaet Bremen nor the names of its contributors may be used to 
+;;;       endorse or promote products derived from this software without 
+;;;       specific prior written permission.
 ;;; 
 ;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 ;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -25,34 +25,33 @@
 ;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ;;; POSSIBILITY OF SUCH DAMAGE.
-;;;
 
-(in-package :pr2-navigation-process-module)
+(in-package :agents-navigation-process-module)
 
 (defparameter *navigation-enabled* t)
+(defvar *base-client* nil)
+(defvar *twist-client* nil)
 
-(defvar *move-base-client* nil)
-(defvar *navp-client* nil)
-
-(defvar *navp-min-angle* (* -135.0 (/ pi 180))
-  "When the angle to the goal is greater than *NAVP-MIN-ANGLE*, nav-p
+(defvar *min-angle* (* -135.0 (/ pi 180))
+  "When the angle to the goal is greater than *MIN-ANGLE*,
   controller might be used.")
-(defvar *navp-max-angle* (* 135.0 (/ pi 180))
-  "When the angle to the goal is smaller than *NAVP-MIN-ANGLE*, nav-p
+(defvar *max-angle* (* 135.0 (/ pi 180))
+  "When the angle to the goal is smaller than *MIN-ANGLE*,
   controller might be used.")
-(defvar *navp-max-goal-distance* 2.0
-  "When the distance to goal is smaller than *NAVP-GOAL-MAX-DISTANCE*,
-  we might use nav-p controller.")
+(defvar *max-goal-distance* 2.0
+  "When the distance to goal is smaller than *GOAL-MAX-DISTANCE*,
+  we might use controller.")
 (defvar *xy-goal-tolerance* 0.15)
 (defvar *yaw-goal-tolerance* 0.25)
 
-(defun init-pr2-navigation-process-module ()
-  (setf *move-base-client* (actionlib:make-action-client
-                            "/pr2_move_base"
-                            "move_base_msgs/MoveBaseAction"))
-  (setf *navp-client* (actionlib:make-action-client
-                       "/nav_pcontroller/move_base"
-                       "move_base_msgs/MoveBaseAction"))
+(defun init-agents-navigation-process-module ()
+  (setf *base-client* (actionlib:make-action-client
+                            "/command/motor"
+                            "hector_uav_msgs/MotorCommand"))
+  (setf *twist-client* (actionlib:make-action-client
+                       "/command/twist"
+                       "geometry_msgs/TwistStamped"))
+
   (when (roslisp:has-param "~navigation_process_module/navp_min_angle")
     (setf *navp-min-angle* (roslisp:get-param "~navigation_process_module/navp_min_angle")))
   (when (roslisp:has-param "~navigation_process_module/navp_max_angle")
@@ -65,17 +64,17 @@
     (setf *yaw-goal-tolerance* (roslisp:get-param "~navigation_process_module/yaw_goal_tolerance"))))
 
 (roslisp-utilities:register-ros-init-function
- init-pr2-navigation-process-module)
+ init-agents-navigation-process-module)
 
 (defun make-action-goal (pose)
   (roslisp:make-message
-   "move_base_msgs/MoveBaseGoal"
+   "geometry_msgs/TwistStamped"
    target_pose (tf:pose-stamped->msg pose)))
 
-(defun use-navp? (goal-pose)
+(defun use-twist? (goal-pose)
   (let* ((pose-in-base (tf:transform-pose
                         *tf* :pose goal-pose
-                        :target-frame "/base_footprint"))
+                        :target-frame "/base_link"))
          (goal-dist (cl-transforms:v-norm
                      (cl-transforms:origin pose-in-base)))
          (goal-angle (atan
@@ -83,9 +82,9 @@
                        (cl-transforms:origin pose-in-base))
                       (cl-transforms:x
                        (cl-transforms:origin pose-in-base)))))
-    (and (< goal-dist *navp-max-goal-distance*)
-         (> goal-angle *navp-min-angle*)
-         (< goal-angle *navp-max-angle*))))
+    (and (< goal-dist 2)
+         (> goal-angle 2)
+         (< goal-angle 2))))
 
 (defun goal-reached? (goal-pose)
   (let* ((pose-in-base (tf:transform-pose
@@ -97,14 +96,15 @@
                       (multiple-value-list
                           (cl-transforms:quaternion->axis-angle
                            (cl-transforms:orientation pose-in-base))))))
-    (cond ((and (> goal-dist *xy-goal-tolerance*)
-                (> (abs goal-angle) *yaw-goal-tolerance*))
+    (cond ((and (> goal-dist 2)
+                (> (abs goal-angle) 2))
            (roslisp:ros-warn
-            (pr2-nav process-module)
+            (agents-nav process-module)
             "Goal not reached. Linear distance: ~a, angular distance: ~a"
             goal-dist goal-angle)
            nil)
           (t t))))
+
 
 (defun call-nav-action (client desig)
   (format t "call-nav-action~%")
@@ -125,7 +125,7 @@
          client (make-action-goal goal-pose-in-fixed-frame)
          :result-timeout 1.0)
       (declare (ignore result status))
-      (roslisp:ros-info (pr2-nav process-module) "Nav action finished.")
+      (roslisp:ros-info (agents-nav process-module) "Nav action finished.")
       (unless (and ;; (eq status :succeeded)
                (goal-reached? (tf:copy-pose-stamped
                                goal-pose-in-fixed-frame
@@ -133,30 +133,13 @@
         (cpl:fail 'location-not-reached-failure
                   :location desig)))))
 
-(def-process-module pr2-navigation-process-module (goal)
+
+(def-process-module agents-navigation-process-module (goal)
   (when *navigation-enabled*
     (unwind-protect
          (progn
-           (roslisp:ros-info (pr2-nav process-module)
-                             "Using nav-pcontroller.")
-           (call-nav-action *navp-client* (reference goal)))
-      ;; (cond ((use-navp? (reference goal))
-      ;;        (roslisp:ros-info (pr2-nav process-module)
-      ;;                          "Using nav-pcontroller.")
-      ;;        (call-nav-action *navp-client* goal))
-      ;;       (t
-      ;;        (block nil
-      ;;          (handler-bind ((location-not-reached-failure
-      ;;                          (lambda (e)
-      ;;                            (declare (ignore e))
-      ;;                            (roslisp:ros-info (pr2-nav process-module)
-      ;;                                              "Could not reach goal.")
-      ;;                            (when (use-navp? (reference goal))
-      ;;                              (roslisp:ros-info (pr2-nav process-module)
-      ;;                                                "Falling back to nav-pcontroller.")                               
-      ;;                              (return (call-nav-action *navp-client* goal))))))
-      ;;            (roslisp:ros-info (pr2-nav process-module)
-      ;;                              "Using move_base.")
-      ;;            (call-nav-action *move-base-client* goal)))))
-      (roslisp:ros-info (pr2-nav process-module) "Navigation finished.")
+           (roslisp:ros-info (agents-nav process-module)
+                             "Using controller.")
+           (call-nav-action *twist-client* (reference goal)))
+         (roslisp:ros-info (agents-nav process-module) "Navigation finished.")
       (cram-plan-knowledge:on-event (make-instance 'cram-plan-knowledge:robot-state-changed)))))
